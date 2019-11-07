3a20a7694ce326ce5e41abda0d9c2cd9c345da82
Sam Noh <samnoh21@outlook.com>
2019-11-07 19:34:06 +1300

    chore(mobx-react): clean up

--- mobx-react-demo/src/hooks/useStore.js
+++ mobx-react-demo/src/hooks/useStore.js
@@ -4,7 +4,10 @@ import { Context as storeContext } from 'contexts/storeContext';
 
 const useStore = () => {
     const store = useContext(storeContext);
-    if (!store) throw Error('Store should not be null');
+
+    if (!store) {
+        throw Error('Store should not be null');
+    }
 
     return store;
 };
